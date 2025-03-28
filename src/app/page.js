80e109150ed93714249ee900fec8c57a8113a2f0
LukeyBit <lukas@lukasandersson.com>
2025-03-28 08:48:12 +0100

    Fixed quotations

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen gap-4">
       <h1 className="text-6xl">Our project</h1>
       <p className="text-lg w-1/2 text-justify">
-        Ynnos the Ekamer is a challenging turn-based RPG game that is inspired by the Flash games "Sonny" and "Sonny 2". Made in Unity, the objective of the game is to fight through stages of battles that test your strategic skills, in order to further an exciting story. To do this, you will level-up your characters, buy better equipment, unlock new abilities and create your own strategies.
+        Ynnos the Ekamer is a challenging turn-based RPG game that is inspired by the Flash games <q>Sonny</q> and <q>Sonny 2</q>. Made in Unity, the objective of the game is to fight through stages of battles that test your strategic skills, in order to further an exciting story. To do this, you will level-up your characters, buy better equipment, unlock new abilities and create your own strategies.
         <br />
         <br />
         The game features maps, different abilities, different enemies and different items.
