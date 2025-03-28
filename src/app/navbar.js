37a60270a764951392597c575223a37a460761f3
LukeyBit <lukas@lukasandersson.com>
2025-03-28 13:33:45 +0100

    Made navbar slightly more responsive

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -2,14 +2,14 @@ import Link from 'next/link';
 
 export default function Navbar() {
     return (
-        <div className="bg-gray-800 text-white p-6 justify-start flex drop-shadow-2xl">
-            <h1 className="text-4xl">Team Decisiontree</h1>
-            <div className="justify-items-end space-x-4 text-2xl mx-auto flex flex-row justify-center items-center text-center mr-1">
+        <nav className="bg-gray-800 text-white p-6 justify-start flex drop-shadow-2xl w-screen">
+            <h1 className="lg:text-4xl text-lg ">Team Decisiontree</h1>
+            <div className="justify-items-end space-x-4 lg:text-2xl text-md mx-auto flex flex-row justify-center items-center text-center lg:mr-2 lg:gap-2">
                 <Link href="/" className="hover:text-blue-400">Home</Link>
                 <a href="/team" className="hover:text-blue-400">About us</a>
                 <a href="/sprints" className="hover:text-blue-400">Sprints</a>
                 <a href="/expo" className="hover:text-blue-400">Expo</a>
             </div>
-        </div>
+        </nav>
     );
 }
