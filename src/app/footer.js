442ed861bb725c007cc73ad6b8808ce9ee8b6ed4
LukeyBit <lukas@lukasandersson.com>
2025-04-02 09:47:38 +0200

    Removed side scroll and updated footer tag type

--- src/app/footer.js
+++ src/app/footer.js
@@ -2,7 +2,7 @@
 
 export default function Footer() {
     return (
-        <div className="bg-gray-800 text-white relative w-full text-center flex flex-col md:flex-row items-center justify-center py-4 gap-2">
+        <footer className="bg-gray-800 text-white relative w-full text-center flex flex-col md:flex-row items-center justify-center py-4 gap-2">
             <p className="text-center">
                 © 2025 Team Decisiontree
             </p>
@@ -10,6 +10,6 @@ export default function Footer() {
             <a className="text-blue-200 hover:text-blue-600 dark:text-blue-300" href="mailto:contact@decisiontree.se" target="_blank">contact@decisiontree.se</a>
             <span className="mx-2 md:block hidden">|</span>
             <a className="text-blue-200 hover:text-blue-600 dark:text-blue-300" href="https://github.com/TeamDecisiontree">GitHub</a>
-        </div>
+        </footer>
     )
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased` + " min-w-screen min-h-screen"}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased` + " max-w-screen min-h-screen"}
       >
         <Navbar />
         {children}
